6b4a483a4b4c092c4f766de7087cbce026323d98
lingardd09 <sherlockicker@gmail.com>
2025-01-11 19:02:00 +0900

    원진 - 그리드 3차 완성(웹, 모바일)

--- js/grid.js
+++ js/grid.js
@@ -55,12 +55,87 @@ const grid_info_members = [
   },
 ];
 
+const grid_info_members_mobile = [
+  {
+    num: 1,
+    name: "김윤채",
+    key1: "#전전",
+    key2: "#카이퍼",
+    key3: "#🧳",
+    music: "music",
+  },
+  {
+    num: 2,
+    name: "김원진",
+    key1: "#전전",
+    key2: "#손양원",
+    key3: "#👮‍♀️",
+    music: "music",
+  },
+  {
+    num: 3,
+    name: "한규호",
+    key1: "#전전",
+    key2: "#손양원",
+    key3: "#🌭",
+    music: "music",
+  },
+  {
+    num: 4,
+    name: "성하은",
+    key1: "#전전",
+    key2: "#손양원",
+    key3: "#⚽️",
+    music: "music",
+  },
+  {
+    num: 5,
+    name: "이하린",
+    key1: "#전전",
+    key2: "#RC",
+    key3: "#🧵",
+    music: "music",
+  },
+  {
+    num: 6,
+    name: "이재호",
+    key1: "#전전",
+    key2: "#RC",
+    key3: "#🎧",
+    music: "music",
+  },
+  {
+    num: 7,
+    name: "오연주",
+    key1: "#전전",
+    key2: "#RC",
+    key3: "#💪",
+    music: "music",
+  },
+  {
+    num: 8,
+    name: "박서연",
+    key1: "#전전",
+    key2: "#RC",
+    key3: "#🐑",
+    music: "music",
+  },
+  {
+    num: 9,
+    name: "진희원",
+    key1: "#전전",
+    key2: "#RC",
+    key3: "#🦥",
+    music: "music",
+  },
+];
+
 const likelion_grid = document.getElementById("likelion_grid");
 
-const htmlString = grid_info_members
+const htmlString1 = grid_info_members
   .map(
     (member) => `
-    <div class="grid-member" onclick="alert('짜잔~~~!')">
+      <div class="grid-member" onclick="alert('짜잔~~~!')">
         <div class="grid-info-memoji-box">
             <img
                 class="grid-info-memoji"
@@ -81,9 +156,56 @@ const htmlString = grid_info_members
             <div class="grid-info-name">${member.name}</div>
             <div class="grid-info-footer">- Frontend -</div>
         </div>
+        <p>저는 몽골에서 태어났습니다.</p>
+      </div>
+
     </div>
-`
+    
+    `
   )
   .join("");
 
-likelion_grid.innerHTML = htmlString;
+const htmlString2 = grid_info_members_mobile
+  .map(
+    (member) => `<div class="grid-mobile-member" onclick="alert('짜잔~~~!')">
+        <div class="grid-mobile-pic-box">
+          <img
+            class="grid-mobile-pic"
+            src="./assets/img/grid/memoji/memoji${member.num}.png"
+            alt="memoji"
+          />
+        </div>
+        <div>
+          <div class="grid-mobile-name">${member.name}</div>
+          <div class="grid-member-info">
+            <div class="grid-mobile-keyword">${member.key1}</div>
+            <div class="grid-mobile-keyword">${member.key2}</div>
+            <div class="grid-mobile-keyword">${member.key3}</div>
+          </div>
+        </div>
+        <div class="grid-mobile-music">
+          <div class="grid-mobile-music-title">${member.music}</div>
+          <div class="grid-mobile-music-icon-box">
+            <img
+              class="grid-info-music-note"
+              src="./assets/img/grid/note.png"
+              alt="note"
+            />
+          </div>
+          <p>저는 몽골에서 태어났습니다.</p>
+
+        </div>
+        </div>
+        `
+  )
+  .join("");
+
+window.onresize = function () {
+  if (window.innerWidth < 800) {
+    likelion_grid.innerHTML = htmlString2;
+  } else {
+    likelion_grid.innerHTML = htmlString1;
+  }
+};
+
+window.onresize();
