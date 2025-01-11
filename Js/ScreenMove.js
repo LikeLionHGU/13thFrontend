d60ed1e1764750f8cef63f0a08c78abca8aa6ed7
playoh <wazoski05@gmail.com>
2025-01-12 00:10:20 +0900

    모바일 버젼 Scrolloing 추가

--- Js/ScreenMove.js
+++ Js/ScreenMove.js
@@ -5,9 +5,17 @@ const contact = document.querySelector(".header-contact");
 function goToScroll(name) {
   let location = name.offsetTop;
   window.scrollTo({top: location, behavior: 'smooth'});
-  //console.log(`I am ${name}`);
+  console.log(`I am ${name}`);
 }
 
 home.onclick = () => goToScroll(home);
 about.onclick = () => goToScroll(about);
 contact.onclick = () => goToScroll(contact);
+
+const mobTop = document.querySelector("#mob_top");
+const mobAbout = document.querySelector("#mob_about");
+const mobBot = document.querySelector("#mob_bot");
+
+mobTop.onclick = () => goToScroll(mobTop);
+mobAbout.onclick = () => goToScroll(mobAbout);
+mobBot.onclick = () => goToScroll(mobBot);
