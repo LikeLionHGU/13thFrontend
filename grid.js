1a3a7340af23ea3c6729ffd618859db499092e98
yoonchae <22400147@handong.ac.kr>
2025-01-11 16:00:32 +0900

    최종1

--- grid.js
+++ grid.js
@@ -3,72 +3,72 @@ const info_members=[
         num: 1,
         name: "김윤채",
         key1: "#전전",
-        key2: "#RC",
-        key3: "#🥲",
+        key2: "#카이퍼",
+        key3: "#🧳",
+        music: "music",
+    },
+    {
+        num: 2,
+        name: "김원진",
+        key1: "#전전",
+        key2: "#손양원",
+        key3: "#👮‍♀️",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "한규호",
         key1: "#전전",
-        key2: "#RC",
-        key3: "#🥲",
+        key2: "#손양원",
+        key3: "#🌭",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "성하은",
         key1: "#전전",
-        key2: "#RC",
-        key3: "#🥲",
+        key2: "#손양원",
+        key3: "#⚽️",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "이하린",
         key1: "#전전",
         key2: "#RC",
-        key3: "#🥲",
+        key3: "#🧵",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "이재호",
         key1: "#전전",
         key2: "#RC",
-        key3: "#🥲",
+        key3: "#🎧",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "오연주",
         key1: "#전전",
         key2: "#RC",
-        key3: "#🥲",
+        key3: "#💪",
         music: "music",
     },
     {
         num: 1,
-        name: "김윤채",
+        name: "박서연",
         key1: "#전전",
         key2: "#RC",
-        key3: "#🥲",
+        key3: "#🐑",
         music: "music",
     },
     {
-        num: 1,
-        name: "김윤채",
+        num:1,
+        name: "진희원",
         key1: "#전전",
         key2: "#RC",
-        key3: "#🥲",
-        music: "music",
-    },
-    {
-        num: 1,
-        name: "김윤채",
-        key1: "#전전",
-        key2: "#RC",
-        key3: "#🥲",
+        key3: "#🦥",
         music: "music",
     }
 ]
@@ -76,7 +76,9 @@ const likelion_grid=document.getElementById("likelion-grid");
 const htmlString =info_members
     .map(
         (member) =>`<div class="member">
-        <div class="mobile-pic"></div>
+        <div class="mobile-pic-box">
+        <img class="mobile-pic" src="${member.num}.png">
+        </div>
         <div>
           <div class="mobile-name">${member.name}</div>
           <div class="member-info">
@@ -88,6 +90,7 @@ const htmlString =info_members
         <div class="mobile-music">
           <div class="mobile-music-title">${member.music}</div>
           <div class="mobile-music-icon-box"><img class="mobile-music-icon" src="note.png"></div>
+        </div>
         </div>
         `
     )
