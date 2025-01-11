b01a53fca5e7fe71aba802d1d867569a0a42b7e6
playoh <wazoski05@gmail.com>
2025-01-12 01:05:25 +0900

    Test2

--- Js/ScreenMove.js
+++ Js/ScreenMove.js
@@ -19,5 +19,3 @@ const mobBot = document.querySelector("#mob_bot");
 mobTop.onclick = () => goToScroll(mobTop);
 mobAbout.onclick = () => goToScroll(mobAbout);
 mobBot.onclick = () => goToScroll(mobBot);
-
-// push test
