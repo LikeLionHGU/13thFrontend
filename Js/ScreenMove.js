15256e4c584e491d0d63baf736016765de087598
Gyuho-Han <0203ho@naver.com>
2025-01-13 12:36:59 +0900

    하트 클릭시 랜덤 색깔로 변경

--- Js/ScreenMove.js
+++ Js/ScreenMove.js
@@ -1,6 +1,6 @@
-const home = document.querySelector("#header-home");
-const about = document.querySelector("#header-about");
-const contact = document.querySelector("#header-contact");
+const home = document.querySelector(".header-home");
+const about = document.querySelector(".header-about");
+const contact = document.querySelector(".header-contact");
 
 function goToScroll(name) {
   let location = name.offsetTop;
@@ -8,15 +8,9 @@ function goToScroll(name) {
   console.log(`I am ${name}`);
 }
 
-// home.onclick = () => goToScroll(home);
-// about.onclick = () => goToScroll(about);
-// contact.onclick = () => goToScroll(contact);
-
-const header = document.querySelector("#likelion-header");
-
-home.addEventListener("click", () => window.scrollTo(1000, 1000));
-about.addEventListener("click", () => goToScroll(about));
-contact.addEventListener("click", () => goToScroll(contact));
+home.onclick = () => goToScroll(home);
+about.onclick = () => goToScroll(about);
+contact.onclick = () => goToScroll(contact);
 
 const mobTop = document.querySelector("#mob_top");
 const mobAbout = document.querySelector("#mob_about");
@@ -25,3 +19,13 @@ const mobBot = document.querySelector("#mob_bot");
 mobTop.onclick = () => goToScroll(mobTop);
 mobAbout.onclick = () => goToScroll(mobAbout);
 mobBot.onclick = () => goToScroll(mobBot);
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
+document.getElementById("heart").addEventListener("click", function () {
+  const randomColor = getRandomColor();
+  const path = this.querySelector("path");
+  path.setAttribute("fill", randomColor); 
+});
