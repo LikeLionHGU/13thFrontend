459570de78562a6e7eb8fa104e6ce0dd1fe49479
harin <lhr8440@gmail.com>
2025-01-14 16:26:05 +0900

    grid.js 복구

--- js/grid.js
+++ js/grid.js
@@ -148,6 +148,230 @@ const grid_info_members_mobile = [
   },
 ];
 
+const modal_info_members = [
+  {
+    num: 1,
+    music: "찬란",
+    artist: "나상현씨밴드",
+    name: "김원진",
+    motto: "중요한건 꺾였는데도 그냥 하는 마음",
+    github: "https://github.com/lingard09",
+    introduce: "https://lingard09.github.io/Profile_Page/",
+    instagram: "https://www.instagram.com/kinnwonjin/",
+    profile: "./image/img/1.png",
+    bgImage: "./image/bgImage/1.JPEG",
+  },
+  {
+    num: 2,
+    music: "밝은 빛이 가득해",
+    artist: "WELOVE",
+    name: "김윤채",
+    motto: "항상 최선을 다하자",
+    github: "https://github.com/KimYoonChae",
+    introduce: "https://kimyoonchae.github.io/Profile-page/",
+    instagram: "https://www.instagram.com/yo0n.c/",
+    profile: "./image/img/2.png",
+    bgImage: "./image/bgImage/2.JPG",
+  },
+  {
+    num: 3,
+    music: "Love Never Felt So Good",
+    artist: "Michael Jackson",
+    name: "박서연",
+    motto: "현재를 즐기자",
+    github: "https://github.com/yean1234",
+    introduce: "https://yean1234.github.io/introduce-myself/index.html",
+    instagram: "hhttps://www.instagram.com/boxyean_05",
+    profile: "./image/img/3.png",
+    bgImage: "./image/bgImage/3.JPG",
+  },
+  {
+    num: 4,
+    music: "HAPPY",
+    artist: "데이식스",
+    name: "성하은",
+    motto: "하면 된다 일단 하자 !",
+    github: "https://github.com/Sunghaeun",
+    introduce: "https://sunghaeun.github.io/new-profile/",
+    instagram: "https://www.instagram.com/harlo_ii/",
+    profile: "./image/img/4.png",
+    bgImage: "./image/bgImage/4.JPG",
+  },
+  {
+    num: 5,
+    music: "Mr. Lawrence",
+    artist: "류이치 사카모토",
+    name: "오연주",
+    motto: "그래도 해야지 어떡해",
+    github: "https://github.com/playoh",
+    introduce: "https://lingard09.github.io/Profile_Page/",
+    instagram: "https://www.instagram.com/play0h/",
+    profile: "./image/img/5.png",
+    bgImage: "./image/bgImage/5.PNG",
+  },
+  {
+    num: 6,
+    music: "Antifreeze",
+    artist: "검정치마",
+    name: "이재호",
+    motto: "성실함은 나의 힘",
+    github: "https://github.com/Jaeho777",
+    introduce: "https://jaeho777.github.io/-1-_-/",
+    instagram: "https://www.instagram.com/_olzhho_/",
+    profile: "./image/img/6.png",
+    bgImage: "./image/bgImage/6.JPEG",
+  },
+  {
+    num: 7,
+    music: "FREEDOM",
+    artist: "AKMU",
+    name: "이하린",
+    motto: "뭐든 끝까지!",
+    github: "https://github.com/junnyang",
+    introduce: "https://github.com/junnyang/LikeLion-myprofile",
+    instagram: "https://www.instagram.com/gkfls._.21/",
+    profile: "./image/img/7.png",
+    bgImage: "./image/bgImage/7.JPEG",
+  },
+  {
+    num: 8,
+    music: "Day n nite",
+    artist: "문수진",
+    name: "진희원",
+    motto: "느리지만 끝까지 해내기",
+    github: "https://github.com/JinHeeWond",
+    introduce: "https://github.com/JinHeeWond/new-profile.git",
+    instagram:
+      "https://www.instagram.com/wlsgmldnjs?igsh=MTEyNHIzdWpzNjJhMg%3D%3D&utm_source=qr",
+    profile: "./image/img/8.png",
+    bgImage: "./image/bgImage/8.PNG",
+  },
+  {
+    num: 9,
+    music: "나무",
+    artist: "카더가든",
+    name: "한규호",
+    motto: "매일매일 후회없이 살자",
+    github: "https://github.com/Gyuho-Han",
+    introduce: "https://gyuho-han.github.io/LikeLionIntroWeb/",
+    instagram: "https://www.instagram.com/gyuhohan03/",
+    profile: "./image/img/9.png",
+    bgImage: "./image/bgImage/9.JPG",
+  },
+];
+
+const modal_info_members_mobile = [
+  {
+    num: 1,
+    music: "찬란",
+    artist: "나상현씨밴드",
+    name: "김원진",
+    motto: "중요한건 꺾였는데도 그냥 하는 마음",
+    github: "https://github.com/lingard09",
+    introduce: "https://lingard09.github.io/Profile_Page/",
+    instagram: "https://www.instagram.com/kinnwonjin/",
+    profile: "./image/img/1.jpg",
+    bgImage: "./image/bgImage/1.JPEG",
+  },
+  {
+    num: 2,
+    music: "밝은 빛이 가득해",
+    artist: "WELOVE",
+    name: "김윤채",
+    motto: "항상 최선을 다하자",
+    github: "https://github.com/KimYoonChae",
+    introduce: "https://kimyoonchae.github.io/Profile-page/",
+    instagram: "https://www.instagram.com/yo0n.c/",
+    profile: "./image/img/2.jpg",
+    bgImage: "./image/bgImage/2.JPG",
+  },
+  {
+    num: 3,
+    music: "Love Never Felt So Good",
+    artist: "Michael Jackson",
+    name: "박서연",
+    motto: "현재를 즐기자",
+    github: "https://github.com/yean1234",
+    introduce: "https://yean1234.github.io/introduce-myself/index.html",
+    instagram: "hhttps://www.instagram.com/boxyean_05",
+    profile: "./image/img/3.jpg",
+    bgImage: "./image/bgImage/3.JPG",
+  },
+  {
+    num: 4,
+    music: "HAPPY",
+    artist: "데이식스",
+    name: "성하은",
+    motto: "하면 된다 일단 하자 !",
+    github: "https://github.com/Sunghaeun",
+    introduce: "https://sunghaeun.github.io/new-profile/",
+    instagram: "https://www.instagram.com/harlo_ii/",
+    profile: "./image/img/4.jpg",
+    bgImage: "./image/bgImage/4.JPG",
+  },
+  {
+    num: 5,
+    music: "Mr. Lawrence",
+    artist: "류이치 사카모토",
+    name: "오연주",
+    motto: "그래도 해야지 어떡해",
+    github: "https://github.com/playoh",
+    introduce: "https://lingard09.github.io/Profile_Page/",
+    instagram: "https://www.instagram.com/play0h/",
+    profile: "./image/img/5.jpg",
+    bgImage: "./image/bgImage/5.PNG",
+  },
+  {
+    num: 6,
+    music: "Antifreeze",
+    artist: "검정치마",
+    name: "이재호",
+    motto: "성실함은 나의 힘",
+    github: "https://github.com/Jaeho777",
+    introduce: "https://jaeho777.github.io/-1-_-/",
+    instagram: "https://www.instagram.com/_olzhho_/",
+    profile: "./image/img/6.jpg",
+    bgImage: "./image/bgImage/6.JPEG",
+  },
+  {
+    num: 7,
+    music: "FREEDOM",
+    artist: "AKMU",
+    name: "이하린",
+    motto: "뭐든 끝까지!",
+    github: "https://github.com/junnyang",
+    introduce: "https://github.com/junnyang/LikeLion-myprofile",
+    instagram: "https://www.instagram.com/gkfls._.21/",
+    profile: "./image/img/7.jpg",
+    bgImage: "./image/bgImage/7.JPEG",
+  },
+  {
+    num: 8,
+    music: "Day n nite",
+    artist: "문수진",
+    name: "진희원",
+    motto: "느리지만 끝까지 해내기",
+    github: "https://github.com/JinHeeWond",
+    introduce: "https://github.com/JinHeeWond/new-profile.git",
+    instagram:
+      "https://www.instagram.com/wlsgmldnjs?igsh=MTEyNHIzdWpzNjJhMg%3D%3D&utm_source=qr",
+    profile: "./image/img/8.jpg",
+    bgImage: "./image/bgImage/8.PNG",
+  },
+  {
+    num: 9,
+    music: "나무",
+    artist: "카더가든",
+    name: "한규호",
+    motto: "매일매일 후회없이 살자",
+    github: "https://github.com/Gyuho-Han",
+    introduce: "https://gyuho-han.github.io/LikeLionIntroWeb/",
+    instagram: "https://www.instagram.com/gyuhohan03/",
+    profile: "./image/img/9.jpg",
+    bgImage: "./image/bgImage/9.JPG",
+  },
+];
+
 const likelion_grid = document.getElementById("likelion_grid");
 
 const htmlString1 = grid_info_members
@@ -185,7 +409,7 @@ const htmlString2 = grid_info_members_mobile
   .map(
     (
       member
-    ) => `<div class="grid-mobile-member" onclick="saveMemberNum('${member.num}')">
+    ) => `<div class="grid-mobile-member" onclick="saveMemberNum('${member.num}', '${member}')">
         <div class="grid-mobile-pic-box">
           <img
             class="grid-mobile-pic"
@@ -230,4 +454,34 @@ window.onresize();
 function saveMemberNum(memberNum) {
   console.log("member", memberNum);
   window.localStorage.setItem("num", memberNum);
+  // console.log(modal_info_members[memberNum - 1]);
+  const modal = document.querySelector(".modal");
+  const modalBody = document.querySelector(".modal_body");
+  modal.style.display = "flex";
+  modalBody.style.background = `url('${
+    modal_info_members[memberNum - 1].bgImage
+  }')`;
+  modal
+    .querySelector(".modal_profile_picture > img")
+    .setAttribute("src", modal_info_members[memberNum - 1].profile);
+  modal.querySelector("#modal_profile_name").innerText =
+    modal_info_members[memberNum - 1].name;
+  console.log(modal_info_members[memberNum - 1].name);
+  modal.querySelector("#modal_music_name").innerText =
+    modal_info_members[memberNum - 1].music;
+  modal.querySelector("#modal_music_singer").innerText =
+    modal_info_members[memberNum - 1].artist;
+  modal.querySelector("#modal_music_name").innerText =
+    modal_info_members[memberNum - 1].music;
+  modal.querySelector("#modal_profile_motto").innerText =
+    modal_info_members[memberNum - 1].motto;
+  modal
+    .querySelector("#github")
+    .setAttribute("href", modal_info_members[memberNum - 1].github);
+  modal
+    .querySelector("#introduce")
+    .setAttribute("href", modal_info_members[memberNum - 1].introduce);
+  modal
+    .querySelector("#instagram")
+    .setAttribute("href", modal_info_members[memberNum - 1].instagram);
 }
